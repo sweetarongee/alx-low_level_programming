1d4487130e998561318afd63f5801e322c33eeba
sweetarongee <sweetarongee@gmail.com>
2022-09-22 08:37:52 -0700

    98

---
+++ 0x06-pointers_arrays_strings/0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+/**
+ *
+ *  * reset_to_98 - updates the value a pointer to an int points to to 98
+ *
+ *   * @n: integer pointed to
+ *
+ *    *
+ *
+ *     * Return: void
+ *
+ *      */
+
+void reset_to_98(int *n)
+
+{
+
+		*n = 98;
+}
