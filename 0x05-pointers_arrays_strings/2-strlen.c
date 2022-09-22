83b4e5a21d2b6338fbfade897fb29a4d33bfb59d
sweetarongee <sweetarongee@gmail.com>
2022-09-22 09:02:19 -0700

    wweee

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,38 @@
+/**
+ *
+ *  * _strlen - returns the length of a string
+ *
+ *   * @s: string s
+ *
+ *    *
+ *
+ *     * Return: length of s
+ *
+ *      */
+
+int _strlen(char *s)
+
+{
+
+		int len = 0;
+
+			int i = 0;
+
+
+
+				for (i = 0; s[i] != '\0'; i++)
+
+						{
+
+									len++;
+
+										}
+
+
+
+					return (len);
+
+}
+
+
+
