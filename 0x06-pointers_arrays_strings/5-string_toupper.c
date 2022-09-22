5cca22f77d9f12dd61ad6293d0bae9b5905d6fa3
sweetarongee <sweetarongee@gmail.com>
2022-09-22 07:27:56 -0700

    vim

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -17,33 +17,29 @@
  *       */
 
 char *string_toupper(char *str)
-
+	
 {
-
+	
 		int index = 0;
-
-
-
+		
+		
+		
 			while (str[index])
-
+				
 					{
-
+						
 								if (str[index] >= 'a' && str[index] <= 'z')
-
+									
 												str[index] -= 32;
-
-
-
+								
+								
+								
 										index++;
-
+										
 											}
-
-
-
+			
+			
+			
 				return (str);
-
-}
-
-Footer
-
-© 2022 GitHub, Inc.
+				
+}.
