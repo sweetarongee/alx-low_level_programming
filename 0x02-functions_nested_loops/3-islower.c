257dd86716815cb7472fffb75077d30bc8845265
sweetarongee <sweetarongee@gmail.com>
2022-09-24 14:09:43 -0700

    pray

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+/**
+ *
+ *  * _islower - checks if a character is lowercase
+ *
+ *   * @c: character to check
+ *
+ *    *
+ *
+ *     * Return: 1 if character is lower, 0 otherwise
+ *
+ *      */
+
+int _islower(int c)
+
+{
+
+		if (c >= 97 && c <= 122)
+
+					return (1);
+
+			return (0);
+}
+
