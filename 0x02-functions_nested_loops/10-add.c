5c611e2f19f36f0f34e7d365848241c0a0c57c89
sweetarongee <sweetarongee@gmail.com>
2022-09-24 14:29:28 -0700

    fd

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+/**
+ *
+ *  * add - returns the sum of two integers
+ *
+ *   * @x: one integer
+ *
+ *    * @y: another integer
+ *
+ *     *
+ *
+ *      * Return: sum of x and y
+ *
+ *       */
+
+int add(int x, int y)
+
+{
+
+		return (x + y);
+
+}
+
