cea871201eb59bc0432f02c0fa5a9e6b0eb19c22
sweetarongee <sweetarongee@gmail.com>
2022-09-24 04:31:59 -0700

    Wee

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#include "main.h"
+
+
+
+/**
+ *
+ *  * main - prints _putchar, followed by a new line
+ *
+ *   *
+ *
+ *    * This program uses a user-defined function to print
+ *
+ *     *
+ *
+ *      * Return: 0
+ *
+ *       */
+
+int main(void)
+
+{
+
+		char s[] = "_putchar";
+
+			int i;
+
+
+
+				for (i = 0; s[i] != '\0'; i++)
+
+							_putchar(s[i]);
+
+
+
+					_putchar('\n');
+
+
+
+						return (0);
+
+}
+
+
+
+Footer
+
+
