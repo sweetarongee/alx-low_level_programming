887d23567618e461fefbc8781a82e6fd75123d0a
sweetarongee <sweetarongee@gmail.com>
2022-09-24 14:16:07 -0700

    vvv

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+/**
+ *
+ *  * _isalpha - checks for alphabetic character
+ *
+ *   * @c: character to check
+ *
+ *    *
+ *
+ *     * Return: 1 if c is a letter, 0 otherwise
+ *
+ *      */
+
+int _isalpha(int c)
+
+{
+
+		if ((c > 64 && c < 91) || (c > 96 && c < 123))
+
+					return (1);
+
+
+
+			return (0);
+
+}
+
