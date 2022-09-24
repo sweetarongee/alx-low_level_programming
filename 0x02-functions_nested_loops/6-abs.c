84bf154f4c7772330538c0f336e04a0a542f0103
sweetarongee <sweetarongee@gmail.com>
2022-09-24 14:20:00 -0700

    fd

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,34 @@
+/**
+ *
+ *  * _abs - computes the absolute value of an integer
+ *
+ *   * @n: integer to compute its absolute value
+ *
+ *    *
+ *
+ *     * Return: absolute value of n
+ *
+ *      */
+
+int _abs(int n)
+
+{
+
+		int abs_val;
+
+
+
+			if (n >= 0)
+
+						abs_val = n;
+
+				else
+
+							abs_val = -1 * n;
+
+
+
+					return (abs_val);
+
+}
+
