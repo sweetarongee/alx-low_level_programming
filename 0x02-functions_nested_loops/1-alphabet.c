f91e2e9c30844c9f6f197c33ab41f487595b7be1
sweetarongee <sweetarongee@gmail.com>
2022-09-24 04:37:14 -0700

    alid

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+#include "main.h"
+
+
+
+/**
+ *
+ *  * print_alphabet - prints the lowercase alphabets
+ *
+ *   *
+ *
+ *    * Return: void
+ *
+ *     */
+
+void print_alphabet(void)
+
+{
+
+		char low;
+
+
+
+			for (low = 'a'; low <= 'z'; low++)
+
+						_putchar(low);
+
+
+
+				_putchar('\n');
+
+}
+
+Footer
+
+#include <stdio.h>
+
+#include "main.h"
+
+
+
+/**
+ *
+ *  * print_alphabet - prints the lowercase alphabets
+ *
+ *   *
+ *
+ *    * Return: void
+ *
+ *     */
+
+void print_alphabet(void)
+
+{
+
+		char low;
+
+
+
+			for (low = 'a'; low <= 'z'; low++)
+
+						_putchar(low);
+
+
+
+				_putchar('\n');
+
+}
+
+Footer
+
+
